7b416b8a6c8929750a026bf13fa9033eab268e36
juan2ramos <juan2ramos@gmail.com>
2015-05-02 18:07:44 -0500

    + scroll + NUmber fixed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,59 @@
-$(function(){
-    SyntaxHighlighter.all();
+
+// Config
+// ================================================
+var $root          = $('html'),
+    $home          = $('#home'),
+    homeHeight     = $home .height(),
+    $number        = $('.Number'),
+    eventType      = ((document.ontouchstart !== null) ? 'click' : 'touchstart');
+
+// Methods
+// =================================================
+
+function navSlide() {
+    var scroll_top = $(window).scrollTop();
+
+    if (scroll_top >= homeHeight) {
+        $number.addClass('is-sticky');
+    } else {
+        $number.removeClass('is-sticky');
+    }
+}
+
+function menuToggle() {
+    if($nav_header.hasClass('is-open')) {
+        $root.removeClass('pinned');
+        $nav_header.removeClass('is-open');
+        $navicon.removeClass('is--closed');
+    } else {
+        $root.addClass('pinned');
+        $nav_header.addClass('is-open');
+        $navicon.addClass('is--closed')
+    }
+}
+
+function anchorScroll(event) {
+    var id     = $(this).attr("href"),
+        target = $(id).offset().top;
+
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+
+    event.preventDefault();
+}
+
+// Handlers
+// =================================================
+
+$('.scrollto').on(eventType, function() {
+    anchorScroll.call(this, event);
 });
+
+$(window).scroll(navSlide);
+
+// Banner Slide
+// =================================================
 $(window).load(function(){
     $('.flexslider').flexslider({
         animation: "slide",
@@ -8,4 +61,5 @@ $(window).load(function(){
             $('body').removeClass('loading');
         }
     });
+    navSlide();
 });
